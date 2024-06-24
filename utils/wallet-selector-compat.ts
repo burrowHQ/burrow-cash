294a2b7bfb59f053caa10780abe724320ee2b7f6
xieqiancaosissi <nature@ref.finance>
2024-06-24 11:46:53 +0800

    add mintbase wallet (#73)

--- utils/wallet-selector-compat.ts
+++ utils/wallet-selector-compat.ts
@@ -10,6 +10,7 @@ import { setupNeth } from "@near-wallet-selector/neth";
 import { setupNearMobileWallet } from "@near-wallet-selector/near-mobile-wallet";
 import { setupModal } from "@near-wallet-selector/modal-ui";
 import { setupLedger } from "@near-wallet-selector/ledger";
+import { setupMintbaseWallet } from "@near-wallet-selector/mintbase-wallet";
 import type { WalletSelectorModal } from "@near-wallet-selector/modal-ui";
 import { Near } from "near-api-js/lib/near";
 import { Account } from "near-api-js/lib/account";
@@ -118,6 +119,11 @@ export const getWalletSelector = async ({ onAccountChange }: GetWalletSelectorAr
         },
       }),
       setupLedger(),
+      setupMintbaseWallet({
+        walletUrl: "https://wallet.mintbase.xyz",
+        contractId: LOGIC_CONTRACT_NAME,
+        deprecated: false,
+      }),
       setupOKXWallet({}),
     ],
     network: defaultNetwork,
