2e522a7da7ae4a2453ebe43231e0613fe2a99ed0
xieqiancaosissi <nature@ref.finance>
2024-07-01 21:37:15 +0800

    Eth top (#77)
    * add eth to top
    
    * update ui

--- components/popup/index.tsx
+++ components/popup/index.tsx
@@ -12,7 +12,7 @@ const Popup = ({ className }) => {
   const [show, setShow] = useState<boolean>(false);
 
   useEffect(() => {
-    if (INCENTIVE_POPUP_STATUS === "3") {
+    if (INCENTIVE_POPUP_STATUS === "4") {
       setShow(false);
     } else {
       setShow(true);
@@ -21,7 +21,7 @@ const Popup = ({ className }) => {
 
   function closePopup() {
     setShow(false);
-    localStorage.setItem("INCENTIVE_POPUP_STATUS", "3");
+    localStorage.setItem("INCENTIVE_POPUP_STATUS", "4");
   }
   if (!show) return null;
   return (
