ad653207f1bec2c8b73ea0ff7c2b873ea99a67b3
nature <nature@ref.finance>
2024-08-02 16:34:40 +0800

    fix cache sort

--- hooks/useTableSorting.ts
+++ hooks/useTableSorting.ts
@@ -5,11 +5,13 @@ import { IOrder, setTableSorting } from "../redux/appSlice";
 export function useTableSorting() {
   const sorting = useAppSelector(getTableSorting);
   const dispatch = useAppDispatch();
-
   const setSorting = (name: string, property: string, order: IOrder) => {
     dispatch(setTableSorting({ name, property, order }));
   };
-
+  if (!sorting.market.property.includes("Money")) {
+    sorting.market.property = "totalSupplyMoney";
+    setSorting("market", "totalSupplyMoney", sorting.market.order);
+  }
   return {
     sorting,
     setSorting,
