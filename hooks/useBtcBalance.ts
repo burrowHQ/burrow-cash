a1cf38d316719191b0abc35971311bd15abdb83c
naturexie <786281870@qq.com>
2024-10-15 17:25:51 +0800

    chore: remove package

--- hooks/useBtcBalance.ts
+++ hooks/useBtcBalance.ts
@@ -1,19 +1,14 @@
 import { useEffect, useState } from "react";
-import Big from "big";
+import Decimal from "decimal.js";
 import { useBtcWalletSelector } from "btc-wallet";
 
 export function useBtcAction({ updater }: any) {
   const [balance, setBalance] = useState<string | null>(null);
   const btcSelector = useBtcWalletSelector();
-  // provider.getBalance().then((res: any) => {
-  //   const _balance = new Big(res.total).div(10 ** 8).toString();
-  //   setBalance(_balance);
-  // });
-  // console.log('777777777777-btcSelector?.account', btcSelector?.account)
   useEffect(() => {
     if (btcSelector?.account) {
       btcSelector.getBalance().then((res) => {
-        const _balance = new Big(res.total).div(10 ** 8).toString();
+        const _balance = new Decimal(res.total).div(10 ** 8).toString();
         setBalance(_balance);
         // console.log('0000000000000-', res);
       });
