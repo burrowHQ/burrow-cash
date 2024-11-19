2a465ed157ada4bcae594496eabfcbbd25b24fe2
deep-path <476044723@qq.com>
2024-11-19 11:09:33 +0800

    feat: add margin interface

--- utils/config.ts
+++ utils/config.ts
@@ -103,7 +103,7 @@ const getConfig = (env: string = defaultNetwork) => {
         liquidationUrl: "https://dev.data-service.ref-finance.com",
         recordsUrl: "https://dev-indexer.ref-finance.com",
         txIdApiUrl: "https://api-testnet.nearblocks.io",
-        marginTradingUrl: " https://testnet-indexer.ref-finance.com",
+        marginTradingUrl: "https://testnet-indexer.ref-finance.com",
         SPECIAL_REGISTRATION_TOKEN_IDS: [
           "3e2210e1184b45b64c8a434c0a7e7b23cc04ea7eb7a6c3c32520d03d4afcb8af",
         ],
