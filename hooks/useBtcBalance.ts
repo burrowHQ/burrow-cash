01486b2bb904fb2af3186e6f4abb00482e0f995c
naturexie <786281870@qq.com>
2024-11-28 12:00:24 +0800

    feat: update btc balance

--- hooks/useBtcBalance.ts
+++ hooks/useBtcBalance.ts
@@ -1,17 +1,21 @@
 import { useEffect, useState } from "react";
 import { useDebounce } from "react-use";
 import Decimal from "decimal.js";
-import { useBtcWalletSelector } from "btc-wallet";
+import { useBtcWalletSelector, getBtcBalance } from "btc-wallet";
 
 export function useBtcAction({ updater }: any) {
-  const [balance, setBalance] = useState<string | null>(null);
+  const [balance, setBalance] = useState<number>(0);
   const btcSelector = useBtcWalletSelector();
   useDebounce(
     () => {
       if (btcSelector?.account) {
-        btcSelector.getBalance().then((res) => {
-          const _balance = new Decimal(res).div(10 ** 8).toString();
-          setBalance(_balance);
+        // btcSelector.getBalance().then((res) => {
+        //   const _balance = new Decimal(res).div(10 ** 8).toString();
+        //   setBalance(_balance);
+        // });
+        getBtcBalance().then((res) => {
+          const { rawBalance, balance: btcBalance } = res;
+          setBalance(btcBalance);
         });
       }
     },
