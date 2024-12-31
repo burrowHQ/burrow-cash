50229a55d16cdedcacbe6d4c83c8eaf72b16124c
lq0-github <1441665200@qq.com>
2024-12-31 10:55:47 +0800

    Fix History 	Size

--- screens/Trading/components/Table.tsx
+++ screens/Trading/components/Table.tsx
@@ -441,7 +441,14 @@ const TradingTable = ({
                                   ),
                                 ),
                               )
-                            : "-"}
+                            : null}
+                          {record.amount_d > 0 && record.amount_p > 0
+                            ? record.trend === "long"
+                              ? getSymbolById(assetD.token_id, assetD.metadata?.symbol)
+                              : record.trend === "short"
+                              ? getSymbolById(assetP.token_id, assetP.metadata?.symbol)
+                              : ""
+                            : null}
                         </td>
                         <td>
                           $
