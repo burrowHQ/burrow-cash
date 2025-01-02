6a45b6114212c8c6eb2538fe33b12e41c6cf355a
deep-path <476044723@qq.com>
2025-01-02 14:23:06 +0800

    fix: remove comment

--- screens/Trading/components/Table.tsx
+++ screens/Trading/components/Table.tsx
@@ -467,19 +467,6 @@ const TradingTable = ({
                     const assetD = getAssetById(record.token_d);
                     const assetP = getAssetById(record.token_p);
                     const assetC = getAssetById(record.token_c);
-                    // const isFilter = [
-                    //   `${getSymbolById(assetP.token_id, assetP.metadata?.symbol)}/${getSymbolById(
-                    //     assetD.token_id,
-                    //     assetD.metadata?.symbol,
-                    //   )}`,
-                    //   `${getSymbolById(assetD.token_id, assetD.metadata?.symbol)}/${getSymbolById(
-                    //     assetP.token_id,
-                    //     assetP.metadata?.symbol,
-                    //   )}`,
-                    // ].includes(filterTitle);
-                    // if (filterTitle && !isFilter) {
-                    //   return null;
-                    // }
                     return (
                       <tr key={index}>
                         <td className="py-5 pl-5">
@@ -997,19 +984,6 @@ const TradingTable = ({
               const assetD = getAssetById(record.token_d);
               const assetP = getAssetById(record.token_p);
               const assetC = getAssetById(record.token_c);
-              // const isFilter = [
-              //   `${getSymbolById(assetP.token_id, assetP.metadata?.symbol)}/${getSymbolById(
-              //     assetD.token_id,
-              //     assetD.metadata?.symbol,
-              //   )}`,
-              //   `${getSymbolById(assetD.token_id, assetD.metadata?.symbol)}/${getSymbolById(
-              //     assetP.token_id,
-              //     assetP.metadata?.symbol,
-              //   )}`,
-              // ].includes(filterTitle);
-              // if (filterTitle && !isFilter) {
-              //   return null;
-              // }
               return (
                 <div className="bg-gray-800 rounded-xl mb-4" key={index}>
                   <div className="pt-5 px-4 pb-4 border-b border-dark-950 flex justify-between">
