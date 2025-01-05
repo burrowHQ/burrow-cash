6c511ade53023382ae9e86fdeaa71593af2cd34a
naturexie <786281870@qq.com>
2025-01-05 17:48:07 +0800

    fix: remove tip for meme

--- components/Modal/CollateralTypeName.tsx
+++ components/Modal/CollateralTypeName.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import HtmlTooltip from "../common/html-tooltip";
 import { Star } from "./svg";
+import { isMemeCategory } from "../../redux/categorySelectors";
+import { useAppSelector } from "../../redux/hooks";
 
 export default function CollateralTypeName({ children }) {
   const [showTooltip, setShowTooltip] = useState(localStorage.getItem("COLLATERAL_TYPE") !== "1");
+  const isMeme = useAppSelector(isMemeCategory);
   function close() {
     localStorage.setItem("COLLATERAL_TYPE", "1");
     setShowTooltip(false);
@@ -14,10 +17,14 @@ export default function CollateralTypeName({ children }) {
       onOpen={() => setShowTooltip(true)}
       onClose={close}
       title={
-        <div className="flex items-start gap-1 text-sm text-gray-1200 w-40">
-          <Star className="relative top-1 flex-shrink-0" />
-          <span>Try different collateral positions.</span>
-        </div>
+        isMeme ? (
+          ""
+        ) : (
+          <div className="flex items-start gap-1 text-sm text-gray-1200 w-40">
+            <Star className="relative top-1 flex-shrink-0" />
+            <span>Try different collateral positions.</span>
+          </div>
+        )
       }
     >
       <span
