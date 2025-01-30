d8bddb5f6c1f808c7bc6eae6f700119c4a0ac54e
naturexie <786281870@qq.com>
2025-01-30 23:39:26 +0800

    feat: update _app for btc wallet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { Provider } from "react-redux";
+import { BtcWalletSelectorContextProvider } from "btc-wallet";
 import type { AppProps } from "next/app";
 import { PersistGate } from "redux-persist/integration/react";
 import { ErrorBoundary } from "@sentry/react";
@@ -15,15 +16,17 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <ErrorBoundary fallback={FallbackError}>
       <ProcessBar />
       <BlockContryTip />
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Head>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <title>Burrow Finance</title>
-          </Head>
-          <Upgrade Component={Component} pageProps={pageProps} />
-        </PersistGate>
-      </Provider>
+      <BtcWalletSelectorContextProvider>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <Head>
+              <meta name="viewport" content="width=device-width, initial-scale=1" />
+              <title>Burrow Finance</title>
+            </Head>
+            <Upgrade Component={Component} pageProps={pageProps} />
+          </PersistGate>
+        </Provider>
+      </BtcWalletSelectorContextProvider>
     </ErrorBoundary>
   );
 }
