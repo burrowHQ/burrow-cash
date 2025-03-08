5acea312d2bb3a92d39f2760b23ffa41f678c120
naturexie <786281870@qq.com>
2025-03-08 16:52:42 +0800

    feat: add meta data

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,14 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
           rel="stylesheet"
         />
+        <meta
+          name="description"
+          content="The first DEX on NEAR. The Chain-Abstracted Liquidity Solution. NEAR wallet to store, buy, send and stake assets for DeFi."
+        />
+        <meta
+          name="keywords"
+          content="NEAR DEX,DEX on NEAR,Top dex on NEAR, DEX, Lending, Borrow, Margin Trading"
+        />
         <link rel="icon" href="/favicon.png" />
         <link rel="apple-touch-icon" href="/favicon.png" />
       </Head>
