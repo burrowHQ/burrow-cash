e3e236c890561d8ecdd8d91aabd48b8afdbe21c7
naturexie <786281870@qq.com>
2025-03-28 11:11:59 +0800

    feat: remove sfrax asset from market

--- utils/config.ts
+++ utils/config.ts
@@ -4,7 +4,11 @@ import { getRpcList } from "../components/Rpc/tool";
 export const LOGIC_CONTRACT_NAME = process.env.NEXT_PUBLIC_CONTRACT_NAME as string;
 export const DUST_THRESHOLD = 0.001;
 
-export const hiddenAssets = ["meta-token.near", "usn"];
+export const hiddenAssets = [
+  "meta-token.near",
+  "usn",
+  "a663b02cf0a4b149d2ad41910cb81e23e1c41c32.factory.bridge.near",
+];
 export const lpTokenPrefix = "shadow_ref_v1";
 
 export const defaultNetwork = (process.env.NEXT_PUBLIC_DEFAULT_NETWORK ||
