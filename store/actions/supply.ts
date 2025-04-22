9f5b66a33fdd767843ac1c682c9068a97be32b7b
naturexie <786281870@qq.com>
2025-04-22 19:25:14 +0800

    fix: nbtc collateral issue

--- store/actions/supply.ts
+++ store/actions/supply.ts
@@ -32,22 +32,24 @@ export async function supply({
   const burrowContractId = isMeme ? logicMEMEContract.contractId : logicContract.contractId;
   let expandedAmount;
   let expandedReceiveAmount;
+  let collateralAmount;
   if (tokenId === NBTCTokenId) {
     expandedAmount = expandTokenDecimal(amount, decimals);
     expandedReceiveAmount = expandTokenDecimal(receiveAmount, decimals);
+    collateralAmount = expandTokenDecimal(expandedReceiveAmount, extraDecimals);
   } else {
     const tokenBalance = new Decimal(await getBalance(tokenId, account.accountId));
     expandedAmount = isMax
       ? tokenBalance
       : decimalMin(expandTokenDecimal(amount, decimals), tokenBalance);
+    collateralAmount = expandTokenDecimal(expandedAmount, extraDecimals);
   }
-  const collateralAmount = expandTokenDecimal(expandedAmount, extraDecimals);
   const collateralActions = {
     actions: [
       {
         IncreaseCollateral: {
           token_id: tokenId,
-          max_amount: collateralAmount.toFixed(0),
+          max_amount: collateralAmount.toFixed(0, Decimal.ROUND_DOWN),
         },
       },
     ],
